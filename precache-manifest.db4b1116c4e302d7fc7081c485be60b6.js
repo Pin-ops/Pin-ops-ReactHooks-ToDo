2ad4540005785265cc874b9aca205e27492b2a0b
Pin-ops <pinozmen89@gmail.com>
2020-04-02 21:14:07 +0200

    Updates

---
+++ precache-manifest.db4b1116c4e302d7fc7081c485be60b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c316f09f2943beaf96614c3dca1c41ac",
+    "url": "/Pin-ops-ReactHooks-ToDo/index.html"
+  },
+  {
+    "revision": "75846496adf244d165d3",
+    "url": "/Pin-ops-ReactHooks-ToDo/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "53bd8583831cbb34e9b4",
+    "url": "/Pin-ops-ReactHooks-ToDo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "75846496adf244d165d3",
+    "url": "/Pin-ops-ReactHooks-ToDo/static/js/2.3137c013.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Pin-ops-ReactHooks-ToDo/static/js/2.3137c013.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53bd8583831cbb34e9b4",
+    "url": "/Pin-ops-ReactHooks-ToDo/static/js/main.1d3624d4.chunk.js"
+  },
+  {
+    "revision": "cc615bd08a750400bbd7",
+    "url": "/Pin-ops-ReactHooks-ToDo/static/js/runtime-main.17662d79.js"
+  }
+]);
